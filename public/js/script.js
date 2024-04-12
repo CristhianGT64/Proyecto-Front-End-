ba70606db1137bfca9cc87d73b0e89aa90c423c1
CristhianGT64 <crislopez_84@hotmail.es>
2024-04-11 18:51:11 -0600

    Se agregaron dos carpetas, una de css y otra de js en public, ya que cuando las queremos usar en resources por alguna razon no funciona, entonces los agregue en la carpeta public, en el archivo de pruebas.blade.php estan las url de como se deben de llamar

---
+++ public/js/script.js
@@ -0,0 +1,6 @@
+// $(document).ready(function(){
+//     alert("Hola")
+//     console.log('Cargado el javaScript');
+// });
+
+console.log('Hola')
--- resources/views/MenuRepartido.blade.php
+++ resources/views/MenuRepartido.blade.php
@@ -8,5 +8,20 @@
 </head>
 <body>
 <h1>Repartidor</h1>
+<div id="map">
+@php
+    // $google_maps_url = 'https://maps.googleapis.com/maps/api/distancematrix/json?destinations=New%20York%20City%2C%20NY&origins=Washington%2C%20DC%7CBoston&units=imperial&key=AIzaSyAZ62cymsGJJcaJV7jZoQyacAuEmP60NE8';
+    // $google_maps_json = file_get_contents($google_maps_url);
+    // $google_maps_array = json_decode($google_maps_json, true);
+    // var_dump($google_maps_array)
+
+    // $ubicacionGoogle = 'https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyAZ62cymsGJJcaJV7jZoQyacAuEmP60NE8';
+    // $UbucacionJson = file_get_contents($ubicacionGoogle);
+    // var_dump($UbucacionJson);
+@endphp
+
+
+
+</div>
 </body>
 </html>
