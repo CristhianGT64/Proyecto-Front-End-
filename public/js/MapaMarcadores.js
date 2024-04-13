40515fba940a1097f48c6dfb387f29d6b0eb75c7
CristhianGT64 <crislopez_84@hotmail.es>
2024-04-13 02:11:43 -0600

    Al mapa se le agrego una circuferencia que delimitara hasta donde podran llegar los repartidores y donde estara los negocios, deberan de ser menores a 3.9 km

--- public/js/MapaMarcadores.js
+++ public/js/MapaMarcadores.js
@@ -1,91 +1,113 @@
 // import { MarkerClusterer } from "https://cdn.skypack.dev/@googlemaps/markerclusterer@2.3.1";
 
-async function initMap() {
-  // Request needed libraries.
-  const { Map, InfoWindow } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary(
-    "marker",
-  );
-  const position = { lat: 14.0857108 , lng: -87.1994419 };//Variable con posicion que usaremos
-
-  //Nueva instancia de un mapa de google
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 3, //zoom incial de nuestra app
-    center: position, //Posicion de nuestro mapa cuando inicie
-    mapId: "DEMO_MAP_ID", // Lo podemos usar para modificar el disenio de nuestro maapa
-  });
-
-    //Es el que nos traera globos de texto para nuestro codigo
-  const infoWindow = new google.maps.InfoWindow({
-    content: "",
-    disableAutoPan: true,
-  });
-  // Crea un arreglo de caracteres alfabéticos utilizados para etiquetar los marcadores
-//   const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  // Añade algunos marcadores al mapa No se usan
-    const markers = locations.map((posiciones) => {
-
-        //Imagen para indicadores de conductores -----
-
-        const beachFlagImg = document.createElement("img"); //creamos una imagen en el html
-        // const beachFlagImg.src = "/imagenes/logoRepartidor.jpg";
-        beachFlagImg.src = "/imagenes/cascoLogo.png"; //decimos a que es igual esa imagen
-        beachFlagImg.width = 18; // Ancho de la imagen en píxeles
-        beachFlagImg.height = 18; // Alto de la imagen en píxeles
-
-        ///Termina imagenes para conductor
-
-    // const label = labels[i % labels.length]; No se usan
-    /* Este de aqui es clave para poner varios marcadores */
-    //Cuando se instancia cada funcion del mapa se debe de poner
-    // su paramentro, funcionan tipo clave valor
-    //Este es para mapear los marcadores, es decir, hacer los dibujos de los marcadores
-    const marker = new google.maps.marker.AdvancedMarkerElement({
-        map,
-        position: posiciones, //Este es el encargado de poner los marcadores
-        title: 'Uluru',
-        content: beachFlagImg, // le decimos que los nuevos inconos seran la imagen que importamos
-    });
+// Función para calcular una nueva posición
+//La distancia debe de recibirse como km, es decir 4,3,2,1
+function CalcularPosicion(lat, lng, distancia, rumbo) {
+    const R = 5000; // Radio de Teguscigalpa y comayaguela aprox en metros
+    const ad = distancia / R; // Convierte la distancia a un ángulo en radianes
+    const lat1 = (Math.PI / 180) * lat; // Convierte la latitud a radianes
+    const lng1 = (Math.PI / 180) * lng; // Convierte la longitud a radianes
+    const rumb = (Math.PI / 180) * rumbo; // Convierte el rumbo a radianes
   
-    // abre la ventana de información cuando se hace clic en el marcador
-    marker.addListener("click", () => {
-      infoWindow.setContent(posiciones.lat + ", " + posiciones.lng); //Este sera modificado para despues poner los nombres de los negocios
-      infoWindow.open(map, marker); //Abre el globo de texto
-      map.setCenter(posiciones);
-      map.setZoom(18);
-    });
-    return marker;
-  });
+    const lat2 = Math.asin(Math.sin(lat1) * Math.cos(ad) + Math.cos(lat1) * Math.sin(ad) * Math.cos(rumb));
+    const lng2 = lng1 + Math.atan2(Math.sin(rumb) * Math.sin(ad) * Math.cos(lat1), Math.cos(ad) - Math.sin(lat1) * Math.sin(lat2));
   
-  // “Añade un agrupador de marcadores para administrar los marcadores
-  new MarkerClusterer({ markers, map });
+    return {
+      lat: ((180 / Math.PI) * lat2), // Convierte la latitud a grados
+      lng: (180 / Math.PI) * lng2, // Convierte la longitud a grados
+    };
+  }
 
-}
+    async function initMap() {
+    // Librerias necesarias para el mapa, map, infoWindows y marker
+    const { Map, InfoWindow } = await google.maps.importLibrary("maps");
+    const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary(
+        "marker",
+    );
+    const position = { lat: 14.0857108 , lng: -87.1994419 };//Variable con posicion que usaremos como
+    //centro de nuestro mapa
 
-const locations = [
-  { lat: 14.0857108 , lng: -87.1994419 },
-  { lat: -33.718234, lng: 150.363181 },
-  { lat: -33.727111, lng: 150.371124 },
-  { lat: -33.848588, lng: 151.209834 },
-  { lat: -33.851702, lng: 151.216968 },
-  { lat: -34.671264, lng: 150.863657 },
-  { lat: -35.304724, lng: 148.662905 },
-  { lat: -36.817685, lng: 175.699196 },
-  { lat: -36.828611, lng: 175.790222 },
-  { lat: -37.75, lng: 145.116667 },
-  { lat: -37.759859, lng: 145.128708 },
-  { lat: -37.765015, lng: 145.133858 },
-  { lat: -37.770104, lng: 145.143299 },
-  { lat: -37.7737, lng: 145.145187 },
-  { lat: -37.774785, lng: 145.137978 },
-  { lat: -37.819616, lng: 144.968119 },
-  { lat: -38.330766, lng: 144.695692 },
-  { lat: -39.927193, lng: 175.053218 },
-  { lat: -41.330162, lng: 174.865694 },
-  { lat: -42.734358, lng: 147.439506 },
-  { lat: -42.734358, lng: 147.501315 },
-  { lat: -42.735258, lng: 147.438 },
-  { lat: -43.999792, lng: 170.463352 },
-];
+    //Nueva instancia de un mapa de google
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 12, //zoom incial de nuestra app
+        center: position, //Posicion de nuestro mapa cuando inicie
+        mapId: "DEMO_MAP_ID", // Lo podemos usar para modificar el disenio de nuestro maapa
+    });
+
+    // Crea un objeto Circle para saber en donde vamos a estar
+    const cityCircle = new google.maps.Circle({
+        strokeColor: "#FF0000", // Color del borde del círculo
+        strokeOpacity: 0.8, // Opacidad del borde
+        strokeWeight: 2, // Grosor del borde
+        fillColor: "#00ff38", // Color de relleno del círculo
+        fillOpacity: 0.2, // Opacidad del relleno
+        map, // Asocia el círculo al mapa que has creado
+        center: position, // Centro del círculo en las coordenadas de latitud y longitud
+        radius: 5000, // Radio del círculo en metros, es el mismo que definimos arriba
+    });
+
+        // Calcula una nueva posición a 50 km al este (90 grados) del centro del 
+        //La distancia lo maximo que podra ser es de 3.9 km
+    const nuevaPosicion = CalcularPosicion(position.lat, position.lng, 3.9, -120);
+
+    //Agregamos un nuevo elemento a nuestra lista, este nos servira para mucho
+    locations.push(nuevaPosicion);
+
+        //Es el que nos traera globos de texto para nuestro mapa cuando toquemos los iconos
+        //Por los son vacios
+    const infoWindow = new google.maps.InfoWindow({
+        content: "",
+        disableAutoPan: true,
+    });
+    // Crea un arreglo de caracteres alfabéticos utilizados para etiquetar los marcadores
+    //   const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    // Añade algunos marcadores al mapa No se usan
+        const markers = locations.map((posiciones) => {
+
+            //Imagen para indicadores de conductores -----
+
+            const beachFlagImg = document.createElement("img"); //creamos una imagen en el html
+            // const beachFlagImg.src = "/imagenes/logoRepartidor.jpg";
+            beachFlagImg.src = "/imagenes/cascoLogo.png"; //decimos a que es igual esa imagen
+            beachFlagImg.width = 23; // Ancho de la imagen en píxeles
+            beachFlagImg.height = 23; // Alto de la imagen en píxeles
+
+            ///Termina imagenes para Repartidor
+
+        // const label = labels[i % labels.length]; No se usan
+        /* Este de aqui es clave para poner varios marcadores */
+        //Cuando se instancia cada funcion del mapa se debe de poner
+        // su paramentro, funcionan tipo clave valor
+        //Este es para mapear los marcadores, es decir, hacer los dibujos de los marcadores
+        const marker = new google.maps.marker.AdvancedMarkerElement({
+            map,
+            position: posiciones, //Este es el encargado de poner los marcadores
+            title: 'Uluru',
+            content: beachFlagImg, // le decimos que los nuevos inconos seran la imagen que importamos
+        });
+    
+        // abre la ventana de información cuando se hace clic en el marcador
+        marker.addListener("click", () => {
+        infoWindow.setContent(posiciones.lat + ", " + posiciones.lng); //Este sera modificado para despues poner los nombres de los negocios
+        infoWindow.open(map, marker); //Abre el globo de texto
+        map.setCenter(posiciones);
+        map.setZoom(16);
+        });
+        return marker;
+    });
+    
+    // “Añade un agrupador de marcadores para administrar los marcadores
+    new MarkerClusterer({ markers, map });
+
+    } /* Final del init map */
+
+    const locations = [
+    { lat: 14.0857108 , lng: -87.1994419 },
+    { lat: 14.099139, lng: -87.192222 },
+    { lat: 14.099750, lng: -87.195111 },
+    { lat: 14.100583, lng: -87.197639 },
+    { lat: 14.086917, lng: -87.193028 },
+    { lat: 14.086778, lng: -87.204306 },
+    ];
 
 initMap(); //Llamado a la funcion
--- resources/views/PruebaMapaMarcadores.blade.php
+++ resources/views/PruebaMapaMarcadores.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script> {{-- Ncesarios js --}}
-    <link rel="stylesheet" type="text/css" href="/css/Mapa.css" /> {{-- Necesario mapa js --}}
+    <link rel="stylesheet" type="text/css" href="/css/app.css" /> {{-- Necesario mapa js --}}
     <title>Prueba de mapa con marcadores</title>
 </head>
 <body>
