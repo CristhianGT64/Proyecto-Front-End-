4939811ba11fca7f1acb7a399447a3db1201ed6b
Eddy Cruz <edjavi177@gmail.com>
2024-04-18 22:47:05 -0500

    borre imagen

--- resources/views/Login.blade.php
+++ resources/views/Login.blade.php
@@ -6,7 +6,6 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Inicio de sesión</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="/public/css/imagenbg.css">
 </head>
 <body class= "bg-info d-flex justify-content-center align-items-center vh-100"> 
     <div class= "bg-white border p-5 rounded-5 text-secondary " style= "width: 25rem">
